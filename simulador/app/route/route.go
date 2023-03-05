89d553ef99c8e6c0dce18a6e7f47f756f477395a
Jefferson Henrique <jhsg.dev@gmail.com>
2023-03-05 07:24:27 -0300

    função obtendo as coordenadas

--- simulador/app/route/route.go
+++ simulador/app/route/route.go
@@ -1,6 +1,12 @@
 package route
 
-import "errors"
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type Route struct {
 	ID        string
@@ -17,4 +23,38 @@ func (r *Route) LoadPositions() error {
 	if r.ID == "" {
 		return errors.New("id da rota não informado!")
 	}
+
+	//criando variavel onde captura o arquivo
+	f, err := os.Open("destinations/" + r.ID + ".txt")
+
+	if err != nil {
+		return err
+	}
+
+	//aguarda tudo da função ser executado pra executar por ultimo o fechamento do arquivo
+	defer f.Close()
+
+	// variavel para ler o conteudo da variavel f
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return err
+		}
+
+		long, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			return err
+		}
+
+		// Adiciona os valores de lat e long do arquivo para a lista de posições
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
+			Long: long,
+		})
+
+	}
+	return nil
 }
