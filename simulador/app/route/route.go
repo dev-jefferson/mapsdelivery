cd7889f0a8bc660b7592665bc293d7ee4a564271
Jefferson Henrique <jhsg.dev@gmail.com>
2023-03-05 06:34:03 -0300

    criando esboço de função que carregara as posições

--- simulador/app/route/route.go
+++ simulador/app/route/route.go
@@ -1,12 +1,20 @@
 package route
 
+import "errors"
+
 type Route struct {
-	ID        string     
-	ClientID  string     
-	Positions []Position 
+	ID        string
+	ClientID  string
+	Positions []Position
 }
 
 type Position struct {
-	Lat  float64 
-	Long float64 
+	Lat  float64
+	Long float64
+}
+
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("id da rota não informado!")
+	}
 }
